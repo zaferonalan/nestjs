b9c7f01f8d8be7b879dce81a6faabb57e7294d62
zafergunayonalan <zafergunay7@gmail.com>
2025-11-04 13:53:54 +0300

    customGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,13 @@ import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import type { AuthenticatedRequestZodDto } from './dto/login-request.dto';
+import { LocalAuthGuard } from './guards/local-auth/local-guard.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('login')
     login(@Request() req: AuthenticatedRequestZodDto) {
         return {
---
+++ src/auth/guards/local-auth/local-guard.guard.spec.ts
@@ -0,0 +1,7 @@
+import { LocalGuardGuard } from './local-guard.guard';
+
+describe('LocalGuardGuard', () => {
+    it('should be defined', () => {
+        expect(new LocalGuardGuard()).toBeDefined();
+    });
+});
